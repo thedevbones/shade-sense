b4e7239d4f7b85b04d2ce4685efc1b71ee94968a
mlagtapon <68876632+mlagtapon@users.noreply.github.com>
2024-11-07 18:02:17 -0600

    Add files necessary to start project

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
-console.log('DEBUG: popup.js loaded');
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    chrome.tabs.query({
+      active: true, 
+      currentWindow: true
+    }, function (queryTabs) {
+      let currTab = queryTabs[0];
+      chrome.tabs.sendMessage(
+        currTab.id, { 
+          thisOne: "changeColor",
+        });
+    });
+  });
+  
+  document.getElementById("darkMode").addEventListener("click", function() {
+    chrome.tabs.query({
+      active: true, 
+      currentWindow: true
+    }, function (queryTabs) {
+      let currTab = queryTabs[0];
+      chrome.tabs.sendMessage(
+        currTab.id, { 
+          makeDark: "darkdark"
+        });
+    });
+  });
+  
---
+++ shade.js
@@ -0,0 +1,44 @@
+//import chroma from "chroma-js";
+
+let color = false;
+let bg = false;
+
+//let chromaColor = chroma('hotpink');
+
+chrome.runtime.onMessage.addListener(
+
+  function (request, sender, sendResponse) {
+    if (request.thisOne === "changeColor") {
+      if (color) {
+        removeColor();
+        color = false;
+      } else {
+        addColor();
+        color = true;
+      }
+    }
+    if (request.makeDark === "darkdark"){
+      if (bg){
+        document.body.style.backgroundColor = "transparent";
+        bg = false;
+      } else {
+        document.body.style.backgroundColor = "black";
+        bg = true;
+      }
+    }
+  }
+);
+
+function addColor() {
+  const filterImage = document.querySelectorAll("img, video"); // what else?
+  for (let i = 0; i < filterImage.length; i++) {
+    filterImage[i].style.filter = "grayscale(100%)";
+  }
+}
+
+function removeColor() {
+  const filterImage = document.querySelectorAll("img, video");
+  for (let i = 0; i < filterImage.length; i++) {
+    filterImage[i].style.filter = "none";
+  }
+}
