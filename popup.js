6d21aa5e55d714e7d0ad12752fd03e75bda6c151
mlagtapon <68876632+mlagtapon@users.noreply.github.com>
2024-12-02 15:29:47 -0600

    added simulator strength

--- popup.js
+++ popup.js
@@ -18,8 +18,114 @@ document.getElementById("tritanomalySim").addEventListener("click", function() {
   sendMessageToActiveTab({ simulation: "tritanomaly" });
 });
 
-document.getElementById("simulatorStrength").addEventListener("input", function(event) {
+/* document.getElementById("simulatorStrength").addEventListener("input", function(event) {
   sendMessageToActiveTab({ simulatorStrength: event.target.value });
+}); */
+
+
+//deut strength buttons
+document.getElementById("deutZeroB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutZero" });
+});
+document.getElementById("deutOneB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutOne" });
+});
+document.getElementById("deutTwoB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutTwo" });
+});
+document.getElementById("deutThreeB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutThree" });
+});
+document.getElementById("deutFourB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutFour" });
+});
+document.getElementById("deutFiveB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutFive" });
+});
+document.getElementById("deutSixB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutSix" });
+});
+document.getElementById("deutSevenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutSeven" });
+});
+document.getElementById("deutEightB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutEight" });
+});
+document.getElementById("deutNineB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutNine" });
+});
+document.getElementById("deutTenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ deutSimulation: "deutTen" });
+});
+
+//prot strength buttons
+document.getElementById("protZeroB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protZero" });
+});
+document.getElementById("protOneB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protOne" });
+});
+document.getElementById("protTwoB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protTwo" });
+});
+document.getElementById("protThreeB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protThree" });
+});
+document.getElementById("protFourB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protFour" });
+});
+document.getElementById("protFiveB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protFive" });
+});
+document.getElementById("protSixB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protSix" });
+});
+document.getElementById("protSevenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protSeven" });
+});
+document.getElementById("protEightB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protEight" });
+});
+document.getElementById("protNineB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protNine" });
+});
+document.getElementById("protTenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ protSimulation: "protTen" });
+});
+
+//trit strength buttons
+document.getElementById("tritZeroB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritZero" });
+});
+document.getElementById("tritOneB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritOne" });
+});
+document.getElementById("tritTwoB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritTwo" });
+});
+document.getElementById("tritThreeB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritThree" });
+});
+document.getElementById("tritFourB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritFour" });
+});
+document.getElementById("tritFiveB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritFive" });
+});
+document.getElementById("tritSixB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritSix" });
+});
+document.getElementById("tritSevenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritSeven" });
+});
+document.getElementById("tritEightB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritEight" });
+});
+document.getElementById("tritNineB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritNine" });
+});
+document.getElementById("tritTenB").addEventListener("click", function() {
+  sendMessageToActiveTab({ tritSimulation: "tritTen" });
 });
 
 ///////////////////////////////
@@ -54,6 +160,14 @@ document.getElementById("imageOnly").addEventListener("change", function(event) 
   sendMessageToActiveTab({ imageOnly: event.target.checked });
 });
 
+document.getElementById("darkMode").addEventListener("change", function(event) {
+  sendMessageToActiveTab({ darkMode: event.target.checked });
+});
+
+document.getElementById("colorPicker").addEventListener("input", function(event) {
+  sendMessageToActiveTab({ overlayColor: event.target.value });
+});
+
 // Function for sending a message to the active tab
 function sendMessageToActiveTab(message) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(queryTabs) {
@@ -61,3 +175,58 @@ function sendMessageToActiveTab(message) {
     chrome.tabs.sendMessage(currTab.id, message);
   });
 }
+
+
+/*
+
+//Deuteranomaly event listener
+document.getElementById("deuteranomaly").addEventListener("click", function() {
+  chrome.tabs.query({
+    active: true, 
+    currentWindow: true
+  }, function (queryTabs) {
+    let currTab = queryTabs[0];
+    chrome.tabs.sendMessage(
+      currTab.id, { 
+        thisOne: "applyDeuteranomaly",
+      });
+  });
+});
+
+
+//Protanomaly event listener
+document.getElementById("protanomaly").addEventListener("click", function() {
+  chrome.tabs.query({
+    active: true, 
+    currentWindow: true
+  }, function (queryTabs) {
+    let currTab = queryTabs[0];
+    chrome.tabs.sendMessage(
+      currTab.id, { 
+        thisOne: "applyProtanomaly",
+      });
+  });
+});
+  
+  document.getElementById("darkMode").addEventListener("click", function() {
+    chrome.tabs.query({
+      active: true, 
+      currentWindow: true
+    }, function (queryTabs) {
+      let currTab = queryTabs[0];
+      chrome.tabs.sendMessage(
+        currTab.id, { 
+          makeDark: "darkdark"
+        });
+    });
+  });
+
+  //hey, this will let the user to pick a color for the overlay
+  document.getElementById("colorPicker").addEventListener("input", function (event) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (queryTabs) {
+        let currTab = queryTabs[0];
+        chrome.tabs.sendMessage(currTab.id, { overlayColor: event.target.value });
+    });
+});
+*/
+
--- shade.js
+++ shade.js
@@ -2,13 +2,13 @@ chrome.runtime.onMessage.addListener(
 
   function (request, sender, sendResponse) {
     if (request.simulation === "deuteranomaly") {
-      applyDeuteranomaly();
+      applyDeuteranomaly(10);
     }
     if (request.simulation === "protanomaly") {
-      applyProtanomaly();
+      applyProtanomaly(10);
     }
     if (request.simulation === "tritanomaly") {
-      applyTritanomaly();
+      applyTritanomaly(10);
     }
     if (request.daltonize === "deuteranomaly") {
       applyDaltonization(1);
@@ -19,6 +19,113 @@ chrome.runtime.onMessage.addListener(
     if (request.daltonize === "tritanomaly") {
       applyDaltonization(3);
     }
+
+    //Deut strength
+    if (request.deutSimulation === "deutZero") {
+      applyDeuteranomaly(0);
+    }
+    if (request.deutSimulation === "deutOne") {
+      applyDeuteranomaly(1);
+    }
+    if (request.deutSimulation === "deutTwo") {
+      applyDeuteranomaly(2);
+    }
+    if (request.deutSimulation === "deutThree") {
+      applyDeuteranomaly(3);
+    }
+    if (request.deutSimulation === "deutFour") {
+      applyDeuteranomaly(4);
+    }
+    if (request.deutSimulation === "deutFive") {
+      applyDeuteranomaly(5);
+    }
+    if (request.deutSimulation === "deutSix") {
+      applyDeuteranomaly(6);
+    }
+    if (request.deutSimulation === "deutSeven") {
+      applyDeuteranomaly(7);
+    }
+    if (request.deutSimulation === "deutEight") {
+      applyDeuteranomaly(8);
+    }
+    if (request.deutSimulation === "deutNine") {
+      applyDeuteranomaly(9);
+    }
+    if (request.deutSimulation === "deutTen") {
+      applyDeuteranomaly(10);
+    }
+
+
+    //Prot strength
+    if (request.protSimulation === "protZero") {
+      applyProtanomaly(0);
+    }
+    if (request.protSimulation === "protOne") {
+      applyProtanomaly(1);
+    }
+    if (request.protSimulation === "protTwo") {
+      applyProtanomaly(2);
+    }
+    if (request.protSimulation === "protThree") {
+      applyProtanomaly(3);
+    }
+    if (request.protSimulation === "protFour") {
+      applyProtanomaly(4);
+    }
+    if (request.protSimulation === "protFive") {
+      applyProtanomaly(5);
+    }
+    if (request.protSimulation === "protSix") {
+      applyProtanomaly(6);
+    }
+    if (request.protSimulation === "protSeven") {
+      applyProtanomaly(7);
+    }
+    if (request.protSimulation === "protEight") {
+      applyProtanomaly(8);
+    }
+    if (request.protSimulation === "protNine") {
+      applyProtanomaly(9);
+    }
+    if (request.protSimulation === "protTen") {
+      applyProtanomaly(10);
+    }
+
+
+    //Trit strength
+    if (request.tritSimulation === "tritZero") {
+      applyTritanomaly(0);
+    }
+    if (request.tritSimulation === "tritOne") {
+      applyTritanomaly(1);
+    }
+    if (request.tritSimulation === "tritTwo") {
+      applyTritanomaly(2);
+    }
+    if (request.tritSimulation === "tritThree") {
+      applyTritanomaly(3);
+    }
+    if (request.tritSimulation === "tritFour") {
+      applyTritanomaly(4);
+    }
+    if (request.tritSimulation === "tritFive") {
+      applyTritanomaly(5);
+    }
+    if (request.tritSimulation === "tritSix") {
+      applyTritanomaly(6);
+    }
+    if (request.tritSimulation === "tritSeven") {
+      applyTritanomaly(7);
+    }
+    if (request.tritSimulation === "tritEight") {
+      applyTritanomaly(8);
+    }
+    if (request.tritSimulation === "tritNine") {
+      applyTritanomaly(9);
+    }
+    if (request.tritSimulation === "tritTen") {
+      applyTritanomaly(10);
+    }
   }
 );
 
@@ -28,7 +135,58 @@ chrome.runtime.onMessage.addListener(
 
 ********************************************************************************************* */
 
-function applyDeuteranomaly() {
+function applyDeuteranomaly(val) {
+  var arr;
+
+  //https://www.inf.ufrgs.br/~oliveira/pubs_files/CVD_Simulation/CVD_Simulation.html
+  const zeroDeut = [1.000000, 0.000000, -0.000000, 0.000000, 1.000000, 0.000000, -0.000000, -0.000000, 1.000000];
+  const oneDeut = [0.866435, 0.177704, -0.044139, 0.049567, 0.939063, 0.011370, -0.003453, 0.007233, 0.996220];
+  const twoDeut = [0.760729, 0.319078, -0.079807, 0.090568, 0.889315, 0.020117, -0.006027, 0.013325, 0.992702];
+  const threeDeut = [0.675425, 0.433850, -0.109275, 0.125303, 0.847755, 0.026942, -0.007950, 0.018572, 0.989378];
+  const fourDeut = [0.605511, 0.528560, -0.134071, 0.155318, 0.812366, 0.032316, -0.009376, 0.023176, 0.986200];
+  const fiveDeut = [0.547494, 0.607765, -0.155259, 0.181692, 0.781742, 0.036566, -0.010410, 0.027275, 0.983136];
+  const sixDeut = [0.498864, 0.674741, -0.173604, 0.205199, 0.754872, 0.039929, -0.011131, 0.030969, 0.980162];
+  const sevenDeut = [0.457771, 0.731899, -0.189670, 0.226409, 0.731012, 0.042579, -0.011595, 0.034333, 0.977261];
+  const eightDeut = [0.422823, 0.781057, -0.203881, 0.245752, 0.709602, 0.044646, -0.011843, 0.037423, 0.974421];
+  const nineDeut = [0.392952, 0.823610, -0.216562, 0.263559, 0.690210, 0.046232, -0.011910, 0.040281, 0.971630];
+  const tenDeut = [0.367322, 0.860646, -0.227968, 0.280085, 0.672501, 0.047413, -0.011820, 0.042940, 0.968881];
+
+  switch (val) {
+    case 0:
+      arr = zeroDeut;
+      break;
+    case 1:
+      arr = oneDeut;
+      break;
+    case 2:
+      arr = twoDeut;
+      break;
+    case 3:
+      arr = threeDeut;
+      break;
+    case 4:
+      arr = fourDeut;
+      break;
+    case 5:
+      arr = fiveDeut;
+      break;
+    case 6:
+      arr = sixDeut;
+      break;
+    case 7:
+      arr = sevenDeut;
+      break;
+    case 8:
+      arr = eightDeut;
+      break;
+    case 9:
+      arr = nineDeut;
+      break;
+    case 10:
+      arr = tenDeut;
+      break;
+  }
+
   const filterImages = document.querySelectorAll("img, video, canvas, svg, iframe, object");
 
   for (let i = 0; i < filterImages.length; i++) {
@@ -38,11 +196,11 @@ function applyDeuteranomaly() {
     //because of cors error
     oneImage.crossOrigin = "Anonymous";
 
-    toDeuteranomaly(oneImage);
+    toDeuteranomaly(oneImage, arr);
   }
 }
 
-function toDeuteranomaly(srcImage) {
+function toDeuteranomaly(srcImage, arr) {
 
   const draw = document.createElement("canvas");
 
@@ -70,12 +228,6 @@ function toDeuteranomaly(srcImage) {
 
   let rgb = grabCanvas.data;
 
-  if (srcImage.origrgb != undefined) {
-    rgb = srcImage.origrgb.slice();
-  } else {
-    srcImage.origrgb = rgb.slice();
-  }
-
   /*
   const deuterMatrix = {
     deuter: [
@@ -92,10 +244,15 @@ function toDeuteranomaly(srcImage) {
     const green = rgb[i + 1];
     const blue = rgb[i + 2];
 
+    /*
     //https://www.geeksforgeeks.org/how-to-multiply-a-3-x-3-matrix-with-a-3-x-1-matrix/
     const newRed = 0.367322 * red + 0.860646 * green + -0.227968 * blue;
     const newGreen = 0.280085 * red + 0.672501 * green + 0.047413 * blue;
     const newBlue = -0.011820 * red + 0.042940 * green + 0.968881 * blue;
+    */
+    const newRed = arr[0] * red + arr[1] * green + arr[2] * blue;
+    const newGreen = arr[3] * red + arr[4] * green + arr[5] * blue;
+    const newBlue = arr[6] * red + arr[7] * green + arr[8] * blue;
 
     rgb[i] = newRed;
     rgb[i + 1] = newGreen;
@@ -133,7 +290,57 @@ function toDeuteranomaly(srcImage) {
 
 ********************************************************************************************* */
 
-function applyProtanomaly() {
+function applyProtanomaly(val) {
+  var arr;
+
+  const zeroProt = [1.000000, 0.000000, -0.000000, 0.000000, 1.000000, 0.000000, -0.000000, -0.000000, 1.000000];
+  const oneProt = [0.856167, 0.182038, -0.038205, 0.029342, 0.955115, 0.015544, -0.002880, -0.001563, 1.004443];
+  const twoPro = [0.734766, 0.334872, -0.069637, 0.051840, 0.919198, 0.028963, -0.004928, -0.004209, 1.009137];
+  const threeProt = [0.630323, 0.465641, -0.095964, 0.069181, 0.890046, 0.040773, -0.006308, -0.007724, 1.014032];
+  const fourProt = [0.539009, 0.579343, -0.118352, 0.082546, 0.866121, 0.051332, -0.007136, -0.011959, 1.019095];
+  const fiveProt = [0.458064, 0.679578, -0.137642, 0.092785, 0.846313, 0.060902, -0.007494, -0.016807, 1.024301];
+  const sixProt = [0.385450, 0.769005, -0.154455, 0.100526, 0.829802, 0.069673, -0.007442, -0.022190, 1.029632];
+  const sevenProt = [0.319627, 0.849633, -0.169261, 0.106241, 0.815969, 0.077790, -0.007025, -0.028051, 1.035076];
+  const eightProt = [0.259411, 0.923008, -0.182420, 0.110296, 0.804340, 0.085364, -0.006276, -0.034346, 1.040622];
+  const nineProt = [0.203876, 0.990338, -0.194214, 0.112975, 0.794542, 0.092483, -0.005222, -0.041043, 1.046265];
+  const tenProt = [0.152286, 1.052583, -0.204868, 0.114503, 0.786281, 0.099216, -0.003882, -0.048116, 1.051998];
+
+  switch (val) {
+    case 0:
+      arr = zeroProt;
+      break;
+    case 1:
+      arr = oneProt;
+      break;
+    case 2:
+      arr = twoPro;
+      break;
+    case 3:
+      arr = threeProt;
+      break;
+    case 4:
+      arr = fourProt;
+      break;
+    case 5:
+      arr = fiveProt;
+      break;
+    case 6:
+      arr = sixProt;
+      break;
+    case 7:
+      arr = sevenProt;
+      break;
+    case 8:
+      arr = eightProt;
+      break;
+    case 9:
+      arr = nineProt;
+      break;
+    case 10:
+      arr = tenProt;
+      break;
+  }
+
   const filterImages = document.querySelectorAll("img, video, canvas, svg, iframe, object");
 
   for (let i = 0; i < filterImages.length; i++) {
@@ -141,12 +348,12 @@ function applyProtanomaly() {
 
     oneImage.crossOrigin = "Anonymous";
 
-    toProtanomaly(oneImage);
+    toProtanomaly(oneImage, arr);
 
   }
 }
 
-function toProtanomaly(srcImage) {
+function toProtanomaly(srcImage, arr) {
 
   const draw = document.createElement("canvas");
 
@@ -178,10 +385,9 @@ function toProtanomaly(srcImage) {
     const green = rgb[i + 1];
     const blue = rgb[i + 2];
 
-    //0.152286 added 1 to see difference
-    const newRed = 1.152286 * red + 1.052583 * green + -0.204868 * blue;
-    const newGreen = 0.114503 * red + 0.786281 * green + 0.099216 * blue;
-    const newBlue = -0.003882 * red + -0.048116 * green + 1.051998 * blue;
+    const newRed = arr[0] * red + arr[1] * green + arr[2] * blue;
+    const newGreen = arr[3] * red + arr[4] * green + arr[5] * blue;
+    const newBlue = arr[6] * red + arr[7] * green + arr[8] * blue;
 
     rgb[i] = newRed;
     rgb[i + 1] = newGreen;
@@ -215,7 +421,58 @@ function toProtanomaly(srcImage) {
 
 ********************************************************************************************* */
 
-function applyTritanomaly() {
+function applyTritanomaly(val) {
+
+  var arr;
+
+  const zeroTrit = [1.000000, 0.000000, -0.000000, 0.000000, 1.000000, 0.000000, -0.000000, -0.000000, 1.000000];
+  const oneTrit = [0.926670, 0.092514, -0.019184, 0.021191, 0.964503, 0.014306, 0.008437, 0.054813, 0.936750];
+  const twoTrit = [0.895720, 0.133330, -0.029050, 0.029997, 0.945400, 0.024603, 0.013027, 0.104707, 0.882266];
+  const threeTrit = [0.905871, 0.127791, -0.033662, 0.026856, 0.941251, 0.031893, 0.013410, 0.148296, 0.838294];
+  const fourTrit = [0.948035, 0.089490, -0.037526, 0.014364, 0.946792, 0.038844, 0.010853, 0.193991, 0.795156];
+  const fiveTrit = [1.017277, 0.027029, -0.044306, -0.006113, 0.958479, 0.047634, 0.006379, 0.248708, 0.744913];
+  const sixTrit = [1.104996, -0.046633, -0.058363, -0.032137, 0.971635, 0.060503, 0.001336, 0.317922, 0.680742];
+  const sevenTrit = [1.193214, -0.109812, -0.083402, -0.058496, 0.979410, 0.079086, -0.002346, 0.403492, 0.598854];
+  const eightTrit = [1.257728, -0.139648, -0.118081, -0.078003, 0.975409, 0.102594, -0.003316, 0.501214, 0.502102];
+  const nineTrit = [1.278864, -0.125333, -0.153531, -0.084748, 0.957674, 0.127074, -0.000989, 0.601151, 0.399838];
+  const tenTrit = [1.255528, -0.076749, -0.178779, -0.078411, 0.930809, 0.147602, 0.004733, 0.691367, 0.303900];
+
+  switch (val) {
+    case 0:
+      arr = zeroTrit;
+      break;
+    case 1:
+      arr = oneTrit;
+      break;
+    case 2:
+      arr = twoTrit;
+      break;
+    case 3:
+      arr = threeTrit;
+      break;
+    case 4:
+      arr = fourTrit;
+      break;
+    case 5:
+      arr = fiveTrit;
+      break;
+    case 6:
+      arr = sixTrit;
+      break;
+    case 7:
+      arr = sevenTrit;
+      break;
+    case 8:
+      arr = eightTrit;
+      break;
+    case 9:
+      arr = nineTrit;
+      break;
+    case 10:
+      arr = tenTrit;
+      break;
+  }
+
   const filterImages = document.querySelectorAll("img, video, canvas, svg, iframe, object");
 
   for (let i = 0; i < filterImages.length; i++) {
@@ -223,12 +480,12 @@ function applyTritanomaly() {
 
     oneImage.crossOrigin = "Anonymous";
 
-    toTritanomaly(oneImage);
+    toTritanomaly(oneImage, arr);
 
   }
 }
 
-function toTritanomaly(srcImage) {
+function toTritanomaly(srcImage, arr) {
 
   const draw = document.createElement("canvas");
 
@@ -260,9 +517,9 @@ function toTritanomaly(srcImage) {
     const green = rgb[i + 1];
     const blue = rgb[i + 2];
 
-    const newRed = 1.255528 * red + -0.076749 * green + -0.178779 * blue;
-    const newGreen = -0.078411 * red + 0.930809 * green + 0.147602 * blue;
-    const newBlue = 0.004733 * red + 0.691367 * green + 0.303900 * blue;
+    const newRed = arr[0] * red + arr[1] * green + arr[2] * blue;
+    const newGreen = arr[3] * red + arr[4] * green + arr[5] * blue;
+    const newBlue = arr[6] * red + arr[7] * green + arr[8] * blue;
 
     rgb[i] = newRed;
     rgb[i + 1] = newGreen;
@@ -315,11 +572,11 @@ const protanomaly = [0.0, 2.02344, -2.52581, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0];
 const tritanomaly = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -0.395913, 0.801109, 0.0];
 
 //https://vision.psychol.cam.ac.uk/jdmollon/papers/colourmaps.pdf
-function daltonize(srcImage,toDaltonize) {
+function daltonize(srcImage, toDaltonize) {
 
-  if (toDaltonize == 1){
+  if (toDaltonize == 1) {
     toDaltonize = deuteranomaly;
-  } else if (toDaltonize == 2){
+  } else if (toDaltonize == 2) {
     toDaltonize = protanomaly;
   } else {
     toDaltonize = tritanomaly;
@@ -360,25 +617,33 @@ function daltonize(srcImage,toDaltonize) {
     var green = rgb[i + 1];
     var blue = rgb[i + 2];
 
-		L = (17.8824 * red) + (43.5161 * green) + (4.11935 * blue);
-		M = (3.45565 * red) + (27.1554 * green) + (3.86714 * blue);
-		S = (0.0299566 * red) + (0.184309 * green) + (1.46709 * blue);
+    L = (17.8824 * red) + (43.5161 * green) + (4.11935 * blue);
+    M = (3.45565 * red) + (27.1554 * green) + (3.86714 * blue);
+    S = (0.0299566 * red) + (0.184309 * green) + (1.46709 * blue);
     simL = (toDaltonize[0] * L) + (toDaltonize[1] * M) + (toDaltonize[2] * S);
     simM = (toDaltonize[3] * L) + (toDaltonize[4] * M) + (toDaltonize[5] * S);
     simS = (toDaltonize[6] * L) + (toDaltonize[7] * M) + (toDaltonize[8] * S);
 
     r = (0.0809444479 * simL) + (-0.130504409 * simM) + (0.116721066 * simS);
-		g = (-0.0102485335 * simL) + (0.0540193266 * simM) + (-0.113614708 * simS);
-		b = (-0.000365296938 * simL) + (-0.00412161469 * simM) + (0.693511405 * simS);
+    g = (-0.0102485335 * simL) + (0.0540193266 * simM) + (-0.113614708 * simS);
+    b = (-0.000365296938 * simL) + (-0.00412161469 * simM) + (0.693511405 * simS);
     r = (red - r);
     g = (green - g);
     b = (blue - b);
 
-    //TODO: Added 1 to 0.7 and 1.0
+    /*
+    TODO: ORIGINAL VALUES
+    rr = (0.0 * r) + (0.0 * g) + (0.0 * b);
+    gg = (0.7 * r) + (1.0 * g) + (0.0 * b);
+    bb = (0.7 * r) + (0.0 * g) + (1.0 * b);
+    */
+
+    //TODO: Added 0.5 to r to 0.5 to g and 0 to b
     //have to press prot daltonization then deuter daltonization to see effect, why?
     rr = (0.0 * r) + (0.0 * g) + (0.0 * b);
-		gg = (1.7 * r) + (2.0 * g) + (0.0 * b);
-		bb = (1.7 * r) + (0.0 * g) + (2.0 * b);
+    gg = (1.2 * r) + (1.5 * g) + (0.0 * b);
+    bb = (1.2 * r) + (0.0 * g) + (1.0 * b);
+    
     r = rr + red;
     g = gg + green;
     b = bb + blue;
@@ -400,5 +665,5 @@ function daltonize(srcImage,toDaltonize) {
   };
 
   srcImage.src = draw.toDataURL();
-  
+
 }
