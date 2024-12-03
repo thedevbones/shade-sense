08a47465a125a270257a5fe3f5e68172c539f716
devbones <87474953+thedevbones@users.noreply.github.com>
2024-12-03 03:51:24 -0600

    Added styles to front end

--- popup.js
+++ popup.js
@@ -5,21 +5,25 @@
 document.getElementById("noneSim").addEventListener("click", function() {
   saveSetting("simulation", "none");
   sendMessageToActiveTab({ simulation: "none" });
+  updateBackgroundGradient("none");
 });
 
 document.getElementById("deuteranomalySim").addEventListener("click", function() {
   saveSetting("simulation", "deuteraomaly");
   sendMessageToActiveTab({ simulation: "deuteranomaly" });
+  updateBackgroundGradient("deuteranomaly");
 });
 
 document.getElementById("protanomalySim").addEventListener("click", function() {
   saveSetting("simulation", "protanomaly");
   sendMessageToActiveTab({ simulation: "protanomaly" });
+  updateBackgroundGradient("protanomaly");
 });
 
 document.getElementById("tritanomalySim").addEventListener("click", function() {
   saveSetting("simulation", "tritanomaly");
   sendMessageToActiveTab({ simulation: "tritanomaly" });
+  updateBackgroundGradient("tritanomaly");
 });
 
 document.getElementById("simulatorStrength").addEventListener("input", function(event) {
@@ -34,21 +38,25 @@ document.getElementById("simulatorStrength").addEventListener("input", function(
 document.getElementById("noneDal").addEventListener("click", function() {
   saveSetting("daltonize", "none");
   sendMessageToActiveTab({ daltonize: "none" });
+  updateBackgroundGradient("none");
 });
 
 document.getElementById("deuteranomalyDal").addEventListener("click", function() {
   saveSetting("daltonize", "deuteranomaly");
   sendMessageToActiveTab({ daltonize: "deuteranomaly" });
+  updateBackgroundGradient("deuteranomaly");
 });
 
 document.getElementById("protanomalyDal").addEventListener("click", function() {
   saveSetting("daltonize", "protanomaly");
   sendMessageToActiveTab({ daltonize: "protanomaly" });
+  updateBackgroundGradient("protanomaly");
 });
 
 document.getElementById("tritanomalyDal").addEventListener("click", function() {
   saveSetting("daltonize", "tritanomaly");
   sendMessageToActiveTab({ daltonize: "tritanomaly" });
+  updateBackgroundGradient("tritanomaly");
 });
 
 document.getElementById("daltonizationStrength").addEventListener("input", function(event) {
@@ -103,4 +111,29 @@ function sendMessageToActiveTab(message) {
     let currTab = queryTabs[0];
     chrome.tabs.sendMessage(currTab.id, message);
   });
+}
+
+function updateBackgroundGradient(type) {
+  const body = document.body;
+  let gradient;
+
+  switch (type) {
+    case "deuteranomaly":
+        gradient = "linear-gradient(135deg, #86a3cc, #92c778, #d6d66a, #ccc451)";
+        break;
+    case "protanomaly":
+        gradient = "linear-gradient(135deg, #5a6eb4, #c0a45f, #e2d962, #c7a05e)";
+        break;
+    case "tritanomaly":
+        gradient = "linear-gradient(135deg, #f093a7, #fab7d6, #85cbe9, #b0e3e6)";
+        break;
+    default:
+        gradient = "linear-gradient(135deg, #ff7eb3, #ff758f, #fc6e51, #ffcd57, #7be495, #4ac3ff, #7b87ff, #b17fff)";
+        break;
+  }
+
+  // https://www.geeksforgeeks.org/how-to-create-gradient-background-animation-using-html-and-css/
+  body.style.background = gradient;
+  body.style.backgroundSize = "200% 200%";
+  body.style.animation = "gradientShift 10s ease infinite";
 }
